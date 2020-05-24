7eb161ab444a3fd7511d9ec904d59d0e59b03fca
AweBob <33764800+AweBob@users.noreply.github.com>
2020-05-24 16:47:43 -0700

    Progress!

--- bot.py
+++ bot.py
@@ -1,37 +1,34 @@
 
-#from discord import Client as discordClient
-from discord.ext import commands, tasks
-
+from discord.ext import commands, tasks #for the discord bot itself
 from supporting_functions import logEvent, getConstants #For printouts and logging of events
 
 #==================================================================================================================================
 
 bot = commands.Bot(command_prefix=getConstants("PREFIX"))
+loggedMessages = {} #id: [message, author, channel]
 
 #==================================================================================================================================
 
 @bot.event
 async def on_ready():
     logEvent(str(bot.user.display_name), "connected")
-    #start event loop
 
 @bot.event
-async def on_message(message) : #log message
-    foo = message.author
-    bar = message.content
-    twothousand = message.id
-    logEvent(message,"")
+async def on_message(message) :
+    loggedMessages[message.id] = [message.content, message.author, message.channel]
+    logEvent("message" + str(message.id),"logged") #if it was an edit the id will remain the same and this will write over the old info
 
 @bot.event
 async def on_message_edit(messageBefore, messageAfter) : 
-    print("Message Editted.")
-    #print(str(message) + "\n\n" + str(other))
-    print(str(messageBefore.content))
-    print(str(messageAfter.content))
-
+    bot.get_channel(getConstants("BOT_CHANNEL")).send('Message Edit"\nAuthor: ' + messageAfter.author + ' in channel: ' + messageBefore.channel + "\n" + messageBefore + "\n" + messageAfter)
+    loggedMessages[messageAfter.id] = [messageAfter.content, messageAfter.author, messageAfter.channel] #reset the records
+    logEvent("edit"+str(messageAfter.id),"logged") #log it
+    
 @bot.event
 async def on_message_deleted(message) : #DOESN'T WORK
-    print("Message Deleted.")
+    foo = 'bar' #
+
+#Have a looping command every so often checking for messages older then the limit and removing them
 
 #==================================================================================================================================
 
