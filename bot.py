837d705024684c21be76d038c083503e02a322ee
AweBob <33764800+AweBob@users.noreply.github.com>
2020-05-24 17:41:42 -0700

    Now works with message edits, but not message deletions

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 
+from discord import Client as discordClient
 from discord.ext import commands, tasks #for the discord bot itself
 from supporting_functions import logEvent, getConstants #For printouts and logging of events
 
 #==================================================================================================================================
 
 bot = commands.Bot(command_prefix=getConstants("PREFIX"))
+discordClient = discordClient()
 loggedMessages = {} #id: [message, author, channel]
 
 #==================================================================================================================================
@@ -15,14 +17,16 @@ async def on_ready():
 
 @bot.event
 async def on_message(message) :
-    loggedMessages[message.id] = [message.content, message.author, message.channel]
-    logEvent("message" + str(message.id),"logged") #if it was an edit the id will remain the same and this will write over the old info
+    if message.author != bot.user : #if it's not the bot
+        loggedMessages[message.id] = [message.content, message.author, message.channel]
+        logEvent("message" + str(message.id),"logged") #if it was an edit the id will remain the same and this will write over the old info
 
 @bot.event
 async def on_message_edit(messageBefore, messageAfter) : 
-    bot.get_channel(getConstants("BOT_CHANNEL")).send('Message Edit"\nAuthor: ' + messageAfter.author + ' in channel: ' + messageBefore.channel + "\n" + messageBefore + "\n" + messageAfter)
-    loggedMessages[messageAfter.id] = [messageAfter.content, messageAfter.author, messageAfter.channel] #reset the records
-    logEvent("edit"+str(messageAfter.id),"logged") #log it
+    if messageBefore.author != bot.user : #if it's not the bot
+        await bot.get_channel(getConstants("BOT_CHANNEL")).send('Message Edit Alert:\nAuthor: ' + str(messageAfter.author) + ' in channel: #' + str(messageBefore.channel) + " \nBefore: " + str(messageBefore.content) + "\nAfter: " + str(messageAfter.content))
+        loggedMessages[messageAfter.id] = [messageAfter.content, messageAfter.author, messageAfter.channel] #reset the records
+        logEvent("edit"+str(messageAfter.id),"logged") #log it
     
 @bot.event
 async def on_message_deleted(message) : #DOESN'T WORK
